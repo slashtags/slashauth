7a17429c5d9070bb500ad13b7d08953c5c96fd91
flaviomoceri <moceri.flavio@gmail.com>
2023-11-12 12:00:54 +0100

    change port

--- src/server.js
+++ src/server.js
@@ -1,18 +1,19 @@
-const { RPC } = require('@slashtagsdev/slashtags-server')
-const SlashtagsURL = require('@synonymdev/slashtags-url')
+const { RPC } = require("@slashtagsdev/slashtags-server");
+const SlashtagsURL = require("@synonymdev/slashtags-url");
 
 const endpointList = [
   {
-    name: 'authz',
-    svc: 'SlashAuthServer.authz',
-    description: 'Authenticate and authorize a user using signature of server generated nonce'
+    name: "authz",
+    svc: "SlashAuthServer.authz",
+    description:
+      "Authenticate and authorize a user using signature of server generated nonce",
   },
   {
-    name: 'magiclink',
-    svc: 'SlashAuthServer.magiclink',
-    description: 'Request a magic to be sent to the user'
-  }
-]
+    name: "magiclink",
+    svc: "SlashAuthServer.magiclink",
+    description: "Request a magic to be sent to the user",
+  },
+];
 
 const handlersWrappers = {
   /**
@@ -23,7 +24,7 @@ const handlersWrappers = {
    * @returns {object}
    */
   authz: async function ({ publicKey, token }) {
-    return await this.authz({ publicKey, token })
+    return await this.authz({ publicKey, token });
   },
 
   /**
@@ -33,9 +34,9 @@ const handlersWrappers = {
    * @returns {object}
    */
   magiclink: async function ({ publicKey }) {
-    return await this.magiclink(publicKey)
-  }
-}
+    return await this.magiclink(publicKey);
+  },
+};
 
 /**
  * SlashAuthServer
@@ -52,31 +53,31 @@ const handlersWrappers = {
  * @returns {SlashAuthServer}
  */
 class SlashAuthServer {
-  constructor (opts = {}) {
-    if (!opts.keypair) throw new Error('No keypair')
-    if (!opts.authz) throw new Error('No authz')
-    if (!opts.magiclink) throw new Error('No magiclink')
+  constructor(opts = {}) {
+    if (!opts.keypair) throw new Error("No keypair");
+    if (!opts.authz) throw new Error("No authz");
+    if (!opts.magiclink) throw new Error("No magiclink");
 
-    this.authz = opts.authz
-    this.magiclink = opts.magiclink
+    this.authz = opts.authz;
+    this.magiclink = opts.magiclink;
 
-    this.keypair = opts.keypair
+    this.keypair = opts.keypair;
 
     this.rpc = {
-      port: opts.port || 8000,
-      host: opts.host || 'localhost',
-      route: opts.route || 'auth',
-      version: opts.version || 'v0.1',
+      port: opts.port === null ? "" : opts.port || 8000,
+      host: opts.host || "localhost",
+      route: opts.route || "auth",
+      version: opts.version || "v0.1",
       endpointList,
       handler: (ctx) => {
-        const fn = handlersWrappers[ctx.getSvcFn()]
-        if (fn) return fn.call(this, ctx.params)
+        const fn = handlersWrappers[ctx.getSvcFn()];
+        if (fn) return fn.call(this, ctx.params);
 
-        return ctx.failedMethod('INVALID_METHOD')
-      }
-    }
+        return ctx.failedMethod("INVALID_METHOD");
+      },
+    };
 
-    this.server = null
+    this.server = null;
   }
 
   /**
@@ -84,37 +85,37 @@ class SlashAuthServer {
    * @param {string} token
    * @returns {string}
    */
-  formatUrl (token) {
+  formatUrl(token) {
     return SlashtagsURL.format(this.keypair.publicKey, {
       path: `/${this.rpc.version}/${this.rpc.route}`,
-      query: `token=${token}&relay=http://${this.rpc.host}:${this.rpc.port}`
-    })
+      query: `token=${token}&relay=http://${this.rpc.host}:${this.rpc.port}`,
+    });
   }
 
   /**
    * Start rpc server
    * @returns {Promise<void>}
    */
-  async start () {
-    this.server = new RPC({ rpc: this.rpc, keypair: this.keypair })
-    return await this.server.start()
+  async start() {
+    this.server = new RPC({ rpc: this.rpc, keypair: this.keypair });
+    return await this.server.start();
   }
 
   /**
    * Stop rpc server
    * @returns {Promise<void>}
    */
-  async stop () {
-    await this.server?.stop()
+  async stop() {
+    await this.server?.stop();
   }
 
   /**
    * Generate valid keypair
    * @returns {object}
    */
-  static generateKeyPair () {
-    return require('noise-curve-ed').generateKeyPair()
+  static generateKeyPair() {
+    return require("noise-curve-ed").generateKeyPair();
   }
 }
 
-module.exports = SlashAuthServer
+module.exports = SlashAuthServer;
